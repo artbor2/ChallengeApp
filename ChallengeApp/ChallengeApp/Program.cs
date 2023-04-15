8f2cd3fff5ee6ce067fddc8ea9a08b33648cd652
Artur Borkowski <artbor2@gmail.com>
2023-04-15 21:15:05 +0200

    Zadanie domowe dzień czwarty

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool female = true;
+var age = 33;
+
+
+if (female && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine(name + ", lat " + age);
+}
+else if (!female && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("To jest już koniec.");
+}
