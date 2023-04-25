abf82e12f7d9cf7e9d14586796970a446a57880b
Artur Borkowski <artbor2@gmail.com>
2023-04-25 19:26:15 +0200

    Porządki

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,11 +2,7 @@
 
 Employee employee = new Employee("Jan", "Kowalski");
 employee.AddGrade(9f);
-employee.AddGrade("4000  ,11");
+employee.AddGrade("4000,11");
 employee.AddGrade(2);
 employee.AddGrade(6);
 var statistics = employee.GetStatistics();
-var statistics1 = employee.GetStatisticsWithForEach();
-var statistics2 = employee.GetStatisticsWithFor();
-var statistics3 = employee.GetStatisticsWithDoWhile();
-var statistics4 = employee.GetStatisticsWithWhile();
